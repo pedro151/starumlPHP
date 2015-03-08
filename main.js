10ce9968c881cc2733409ce9617fbf314569270b
pedro151 <phacl151>
2015-03-08 20:39:22 -0300

    commit

--- main.js
+++ main.js
@@ -22,7 +22,7 @@
  */
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, regexp: true */
-/*global define, $, _, window, staruml, type, appshell, document */
+/*global define, $, _, window, app, type, appshell, document */
 
 define(function (require, exports, module) {
     "use strict";
