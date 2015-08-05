ed34f56d1c6f47f9aa34c74a9365d7a4e51b49f7
Pedro Alarcão <phacl@hotmail.com>
2015-08-05 18:26:48 -0300

    commit fix
    fix 0.1.1
    *namespace
    *file extencio

--- PHPPreferences.js
+++ PHPPreferences.js
@@ -35,6 +35,12 @@ define(function (require, exports, module) {
             type: "Check",
             default: true
         },
+        "php.gen.phpStrictMode": {
+            text: "Strict Mode",
+            description: "Generate PHP Strict Mode (e.q. PHP7).",
+            type: "Check",
+            default: true
+        },
         "php.gen.useTab": {
             text: "Use Tab",
             description: "Use Tab for indentation instead of spaces.",
@@ -51,13 +57,13 @@ define(function (require, exports, module) {
             text: "Append to class filename",
             description: "Insert value into class filename extensions (e.g. MyClass.class.php)",
             type: "String",
-            default: ".class"
+            default: ""
         },
         "php.gen.interfaceExtension": {
             text: "Append to interface filename",
             description: "Insert value into interface filename extensions (e.g. MyInterface.interface.php)",
             type: "String",
-            default: ".interface"
+            default: ""
         },
         "php.rev": {
             text: "PHP Reverse Engineering",
@@ -105,7 +111,8 @@ define(function (require, exports, module) {
             useTab        : PreferenceManager.get("php.gen.useTab"),
             indentSpaces  : PreferenceManager.get("php.gen.indentSpaces"),
             classExtension : PreferenceManager.get("php.gen.classExtension"),
-            interfaceExtension : PreferenceManager.get("php.gen.interfaceExtension")
+            interfaceExtension : PreferenceManager.get("php.gen.interfaceExtension"),
+            phpStrictMode : PreferenceManager.get("php.gen.phpStrictMode")
         };
     }
 
